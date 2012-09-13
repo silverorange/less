23ea38620d047fbcb9e799632831a8e5713f789b
Michael Gauthier <mike@silverorange.com>
2012-09-13 17:54:15 +0000

    Initial checkin of LESS package.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/less/trunk@74354 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Less/Less.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once 'Swat/SwatHtmlHeadEntrySet.php';
+require_once 'Swat/SwatJavaScriptHtmlHeadEntry.php';
+
+/**
+ * The initial setup of an XML-RPC Ajax component
+ *
+ * @package   Less
+ * @copyright 2012 silverorange
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ */
+class Less
+{
+	/**
+	 * The package identifier
+	 */
+	const PACKAGE_ID = 'LESS';
+
+	/**
+	 * Gets the HTML head entries required by LESS
+	 *
+	 * @return SwatHtmlHeadEntrySet a set of SwatHtmlHeadEntry objects
+	 *                               containing the required JavaScript files
+	 *                               for client-side LESS.
+	 */
+	public static function getHtmlHeadEntrySet()
+	{
+		$set = new SwatHtmlHeadEntrySet();
+
+		$set->addEntry(
+			'packages/less/javascript/less-1.3.0.js',
+			self::PACKAGE_ID
+		);
+
+		return $set;
+	}
+}
+
+?>
+
---
+++ package.php
@@ -0,0 +1,68 @@
+<?php
+
+require_once 'PEAR/PackageFileManager2.php';
+
+$version = '0.0.1';
+$notes = <<<EOT
+see ChangeLog
+EOT;
+
+$description =<<<EOT
+PEAR packaged version of the LESS CSS framework. See
+http://lesscss.org/.
+EOT;
+
+$package = new PEAR_PackageFileManager2();
+PEAR::setErrorHandling(PEAR_ERROR_DIE);
+
+$result = $package->setOptions(
+	array(
+		'filelistgenerator' => 'svn',
+		'simpleoutput'      => true,
+		'baseinstalldir'    => '/',
+		'packagedirectory'  => './',
+		'dir_roles'         => array(
+			'www'           => 'data',
+			'dependencies'  => 'data'
+		)
+	)
+);
+
+$package->setPackage('Less');
+$package->setSummary('LESS CSS Framework');
+$package->setDescription($description);
+$package->setChannel('pear.silverorange.com');
+$package->setPackageType('php');
+$package->setLicense('Apache 2.0', 'http://www.apache.org/licenses/LICENSE-2.0');
+$package->setReleaseVersion($version);
+$package->setReleaseStability('stable');
+$package->setAPIVersion('0.0.1');
+$package->setAPIStability('stable');
+$package->setNotes($notes);
+
+$package->addIgnore('package.php');
+
+$package->addMaintainer(
+	'lead',
+	'gauthierm',
+	'Mike Gauthier',
+	'mike@silverorange.com'
+);
+
+$package->setPhpDep('5.1.5');
+$package->setPearinstallerDep('1.4.0');
+$package->addPackageDepWithChannel(
+	'required',
+	'Swat',
+	'pear.silverorange.com',
+	'1.4.119'
+);
+$package->generateContents();
+
+if (isset($_GET['make']) || (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make')) {
+	$package->writePackageFile();
+} else {
+	$package->debugPackageFile();
+}
+
+?>
